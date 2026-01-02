9549a8241bf91f06e29001242549dea85ea66eb9
Deokgon Kim <dgkim@dgkim.net>
2026-01-02 18:45:21 +0900

    test: update noop

--- apps/notifier/src/handlers/noop.ts
+++ apps/notifier/src/handlers/noop.ts
@@ -1,5 +1,6 @@
 export const handler = async (event: any) => {
   console.log('Noop handler received event:', event);
+  console.log('update test notifier');
 
   return {
     message: 'No operation performed.',
